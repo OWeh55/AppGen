68cce30cea118b89e446542cd6fe1526e596df34
Wolfgang Ortmann <noo@uni-jena.de>
2017-12-22 18:29:37 +0100

    renaming

--- src/filelist.cpp
+++ src/filelist.cpp
@@ -13,14 +13,15 @@ std::string PathAndFilename(const std::string& path, const std::string& filename
   return result;
 }
 
-void FileList(const std::string& fn, std::vector<std::string>& file, bool recursive)
+void FileList(const std::string& fn, 
+	      std::vector<std::string>& file, bool recursive)
 {
-  struct stat fstat;
-  if (stat(fn.c_str(), &fstat) != 0)
+  struct stat fileStatus;
+  if (stat(fn.c_str(), &fileStatus) != 0)
     return;
-  if (S_ISREG(fstat.st_mode))
+  if (S_ISREG(fileStatus.st_mode))
     file.push_back(fn);
-  else if (S_ISDIR(fstat.st_mode))
+  else if (S_ISDIR(fileStatus.st_mode))
     {
       struct dirent*  dp;
       DIR* dir;
@@ -31,15 +32,15 @@ void FileList(const std::string& fn, std::vector<std::string>& file, bool recurs
             {
               std::string fname = dp->d_name;
               std::string pname = PathAndFilename(fn, fname);
-              if (stat(pname.c_str(), &fstat) == 0)
+              if (stat(pname.c_str(), &fileStatus) == 0)
                 {
-                  if (S_ISDIR(fstat.st_mode))
+                  if (S_ISDIR(fileStatus.st_mode))
                     {
                       if (recursive)
                         if (fname != "." && fname != "..")
                           FileList(pname, file, true);
                     }
-                  else if (S_ISREG(fstat.st_mode))
+                  else if (S_ISREG(fileStatus.st_mode))
                     file.push_back(pname);
                 }
             }
